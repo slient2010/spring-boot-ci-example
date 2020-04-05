c4a3790dcb30a4c75cbbb6383140696744a0d3bd
slient2010 <slient2020@163.com>
2020-04-05 17:42:47 +0800

    update ci files

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -5,14 +5,14 @@ gradle clean assemble
 pwd
 ls build/libs/
 # cp build/libs/*.jar ../artifacts/
-cp build/libs/*.jar spring-boot-ci-example
+cp -a build/libs/*.jar spring-boot-ci-example
 cat << EOF > Dockerfile
 
 FROM dev-harbor.teyixing.com/public/java:11.0.3
 
 # 需要变量
 # ADD ./spring-boot-ci-example/build/libs/spring-boot-ci-example-0.0.1-SNAPSHOT.jar /spring-boot-ci-example-0.0.1-SNAPSHOT.jar
-ADD /spring-boot-ci-example/spring-boot-ci-example-0.0.1-SNAPSHOT.jar /spring-boot-ci-example-0.0.1-SNAPSHOT.jar
+ADD spring-boot-ci-example/spring-boot-ci-example-0.0.1-SNAPSHOT.jar /spring-boot-ci-example-0.0.1-SNAPSHOT.jar
 
 ENV NAME=world
 
