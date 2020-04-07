79808169fc6d10197bb91724bae188e4cec7d268
slient2010 <slient2020@163.com>
2020-04-07 20:35:39 +0800

    update scripts

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -4,6 +4,6 @@ ls .
 gradle clean assemble
 pwd
 ls build/libs/
-mkdir docker-builds-binaries
+# mkdir docker-builds-binaries
 # cp build/libs/*.jar ../artifacts/
-cp -a build/libs/*.jar docker-builds-binaries/
+# cp -a build/libs/*.jar docker-builds-binaries/
--- ci/scripts/create-context.sh
+++ ci/scripts/create-context.sh
@@ -3,5 +3,6 @@
 set -e
 
 cp spring-boot-ci-example/ci/docker/Dockerfile context/
+pwd
 cp -R docker-builds-binaries/ context/
 ls -al context/docker-builds-binaries
