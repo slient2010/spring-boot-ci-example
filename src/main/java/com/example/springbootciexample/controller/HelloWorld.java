1fbb6f84932e98525d3e7ae8b25c02cfe32191c2
slient2010 <slient2020@163.com>
2020-04-18 12:23:42 +0800

    update code

--- src/main/java/com/example/springbootciexample/controller/HelloWorld.java
+++ src/main/java/com/example/springbootciexample/controller/HelloWorld.java
@@ -7,7 +7,7 @@ import java.util.Date;
 
 @RestController
 public class HelloWorld {
-    @RequestMapping("/")
+    @RequestMapping("/",method=RequestMethod.GET)
     public String sayHi(){
         //修改返回的字符串的内容
         return "Hi, welcome to my site!" + new Date() ;
