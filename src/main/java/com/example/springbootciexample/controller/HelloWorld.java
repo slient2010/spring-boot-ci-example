55a58904e0459024bef21fe5f6bbae450be52e0a
slient2010 <slient2020@163.com>
2020-04-18 12:34:22 +0800

    update code

--- src/main/java/com/example/springbootciexample/controller/HelloWorld.java
+++ src/main/java/com/example/springbootciexample/controller/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.example.springbootciexample.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
@@ -9,6 +10,7 @@ import java.util.Date;
 public class HelloWorld {
     // @RequestMapping("/",method=RequestMethod.GET)
     @RequestMapping(value={"", "/", "welcome"})}
+    @RequestMapping(value="/", method=RequestMethod.GET)
     public String sayHi(){
         //修改返回的字符串的内容
         return "Hi, welcome to my site!" + new Date() ;
